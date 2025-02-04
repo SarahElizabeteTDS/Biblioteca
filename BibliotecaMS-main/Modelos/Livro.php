1a828c29883f66623b9de32e1bbb5ab824c8a93c
SarahElizabeteTDS <130590547+SarahElizabeteTDS@users.noreply.github.com>
2025-02-04 01:20:25 -0300

    Add files via upload

---
+++ BibliotecaMS-main/Modelos/Livro.php
@@ -0,0 +1,92 @@
+<?php
+
+enum StatusLivro: string{
+    case DISPONIVEL = 'disponivel';
+    case EMPRESTADO = 'emprestado';
+}
+
+abstract class Livro
+{
+    // atributos imutáveis
+    protected int $id;
+    protected string $genero;
+    protected string $titulo;
+    protected string $autor;
+    protected string $editora;
+
+    // atributos mutáveis
+    protected StatusLivro $status;
+
+    //construct
+    public function __construct($id, $g, $tit, $au, $ed, StatusLivro $status = StatusLivro::DISPONIVEL)
+    {
+        $this->id = $id;
+        $this->genero = $g;
+        $this->titulo = $tit;
+        $this->autor = $au;
+        $this->editora = $ed;
+    }
+    
+    public function __toString(): string{
+        return "Livro: {$this->titulo} | Autor: {$this->autor} | Tipo: {$this->getTipo()}";
+    }
+    //->fazer bonitinho com capa //ver se não é melhor fazer em cada um uma capa diferente.
+    //metodos
+     
+    public abstract function getTipo(): string;
+
+    //getssets
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getGenero(): string
+    {
+        return $this->genero;
+    }
+
+    public function getTitulo(): string
+    {
+        return $this->titulo;
+    }
+
+    public function setTitulo(string $titulo): self
+    {
+        $this->titulo = $titulo;
+
+        return $this;
+    }
+
+    public function getAutor(): string
+    {
+        return $this->autor;
+    }
+    
+    public function setAutor(string $autor): self
+    {
+        $this->autor = $autor;
+
+        return $this;
+    }
+    
+    public function getEditora(): string
+    {
+        return $this->editora;
+    }
+
+    public function setEditora(string $editora): self
+    {
+        $this->editora = $editora;
+
+        return $this;
+    }
+
+    public function setarGenero(string $genero):self{
+        if (empty($genero)){
+            throw new InvalidArgumentException("Sem gênero, escolha um");
+        }
+        $this->genero = $genero;
+        return $this;
+    }
+}
